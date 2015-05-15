ab669b641932eaa1d7ae2154a23c15a7b462bda3
JayCSmooth <justchillin4life@gmail.com>
2015-05-15 09:41:24 -0400

    Update RosterProject.java

--- RosterProject.java
+++ RosterProject.java
@@ -2,26 +2,46 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.lang.*;
 public class RosterProject
-
-  
 {
+    public class Roster 
+    {
+        public ArrayList<Student> roster = new ArrayList<Student>();
     
-   
+    
 
-    public void main(String args[])
-    {
+        public void main(String args[])
+        {
       
-        String [] students = {"1,John,Smith,John1989@gmail.com,20,88,79,59",
-				"2,Suzan,Erickson,Erickson_1990@gmailcom,19,91,72,85",
-				"3,Jack,Napoli,The_lawyer99@yahoo.com,19,85,84,87",
-				"4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
-				"5,Jeremy,Brown,jbro332@wgu.edu,27,88,92,84"};
+            String [] students = {"1,John,Smith,John1989@gmail.com,20,88,79,59",
+                "2,Suzan,Erickson,Erickson_1990@gmailcom,19,91,72,85",
+                "3,Jack,Napoli,The_lawyer99@yahoo.com,19,85,84,87",
+                "4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
+                "5,Jeremy,Brown,jbro332@wgu.edu,27,88,92,84"};
         
+                populateRoster();
+        }
         
-    }
+        public void populateRoster()
+        {
+            roster.add(new Student("1", "John", "Smith", "John1989@gmail.com", 20, 88, 79, 59));
+            roster.add(new Student("2", "Suzan", "Erickson", "Erickson_1990@gmailcom", 19, 91, 72, 85));
+            roster.add(new Student("3", "Jack", "Napoli", "The_lawyer99@yahoo.com", 19, 85, 84, 87));
+            roster.add(new Student("4", "Erin", "Black", "Erin.black@comcast.net", 22, 91, 98, 82));
+            roster.add(new Student("5", "Jeremy", "Brown", "jbro332@wgu.edu", 27, 88, 92, 84));
+        
+            public static void add(String studentID, String firstName, String lastName, String emailAddress, int age, int testGrade1, int testGrade2, int testGrade3)
+            
+                String [] students = {studentId,firstName,lastName,emailAddress,age,testGrade1,testGrade2,testGrade3};
+            
+        
     
-    public class Student
-    {
+        Object[] Student = roster.toArray();
+
+        }
+        
+    
+        public class Student
+        {
             public String studentID;
             public String firstName;
             public String lastName;
@@ -31,8 +51,8 @@ public class RosterProject
             public int testGrade2;
             public int testGrade3;
             
-            Student(String studentID, String firstName, String lastName, String emailAddress, int age, int testGrade1, int testGrade2, int testGrade3) {
-                        studentID = studentID;
+            public Student(String studentID, String firstName, String lastName, String emailAddress, int age, int testGrade1, int testGrade2, int testGrade3) 
+            {           studentID = studentID;
                         firstName = firstName;
                         lastName = lastName;
                         emailAddress = emailAddress;
@@ -40,10 +60,10 @@ public class RosterProject
                         testGrade1 = testGrade1;
                         testGrade2 = testGrade2;
                         testGrade3 = testGrade3;
-                    }
+            }
   
     
-    public String getStudentID(String studentID){return studentID;}
+        public String getStudentID(String studentID){return studentID;}
         public String getFirstName(String firstName){return firstName;}
         public String getLastName(String lastName){return lastName;}
         public String getEmailAddress(String emailAddress){return emailAddress;}
@@ -77,30 +97,10 @@ public class RosterProject
                      + ", " + testGrade2 + ", " + testGrade3 + "}");
         }
        
-    public class Roster 
-    {
-    public ArrayList<Student> roster = new ArrayList<Student>();
     
-    public static void add(String studentID, String firstName, String lastName, String emailAddress, int age, int testGrade1, int testGrade2, int testGrade3)
-    {
-        String [] students = {studentId,firstName,lastName,emailAddress,age,testGrade1,testGrade2,testGrade3};
-        
-        
-    }
-    public void populateRoster()
-    {
-        roster.add(new Student("1", "John", "Smith", "John1989@gmail.com", 20, 88, 79, 59));
-        roster.add(new Student("2", "Suzan", "Erickson", "Erickson_1990@gmailcom", 19, 91, 72, 85));
-        roster.add(new Student("3", "Jack", "Napoli", "The_lawyer99@yahoo.com", 19, 85, 84, 87));
-        roster.add(new Student("4", "Erin", "Black", "Erin.black@comcast.net", 22, 91, 98, 82));
-        roster.add(new Student("5", "Jeremy", "Brown", "jbro332@wgu.edu", 27, 88, 92, 84));
-        
-        Object[] Student = roster.toArray();
-
-    }
+    
   
         public int findID(String studentID)
-        
     { 
         for (int i = 0; i < roster.size(); i++)
         {
@@ -125,28 +125,28 @@ public class RosterProject
     }
     
     public static void print_average_grade(String studentID)
+    {
+        for(Student p : roster)
         {
-        for( Student p : roster)
-              {
                     if((studentID) == p.studentID)
                     {
                           float avg = (p.testGrade1 + p.testGrade2 + p.testGrade3) / 3;
                           System.out.println("StudentID # " + studentID + " has an average test grade of " + avg);
                           return;
                     }
-              }
+        }
               System.out.println("The Student ID does not exist in the roster.");
-             }    
-        public static void print_invalid_emails()
-            {
+    }    
+    public static void print_invalid_emails()
+    {
              
-                  for(Student p : roster)
-                  {
-                        if(p.emailAddress.indexOf('@')==-1 || p.emailAddress.indexOf('.')==-1)
-                        {
+         for(Student p : roster)
+         {
+                    if(p.emailAddress.indexOf('@')==-1 || p.emailAddress.indexOf('.')==-1)
+                    {
                               System.out.println("Invalid email address for Student ID: " + p.studentID
                                   + "\tEmail Address: " + p.emailAddress);
                     
-                        }
-                   }
-                }           } }}
+                    }
+         }
+    }         
