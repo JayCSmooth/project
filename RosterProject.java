b32fb7f1d8b529ca8c26c045844cff842b7339fc
JayCSmooth <justchillin4life@gmail.com>
2015-05-20 16:15:27 -0400

    Update RosterProject.java

--- RosterProject.java
+++ RosterProject.java
@@ -3,22 +3,23 @@ import java.util.ArrayList;
 import java.lang.*;
 public class RosterProject
 {
-    public class Roster 
-    {
-        public ArrayList<Student> roster = new ArrayList<Student>();
+     static String [] students = {"1,John,Smith,John1989@gmail.com,20,88,79,59",
+                "2,Suzan,Erickson,Erickson_1990@gmailcom,19,91,72,85",
+                "3,Jack,Napoli,The_lawyer99@yahoo.com,19,85,84,87",
+                "4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
+                "5,Jeremy,Brown,jbro332@wgu.edu,27,88,92,84"};
+                
+                public static ArrayList<Student> roster = new ArrayList<Student>();
     
     
         public void main(String args[])
         {
       
-            String [] students = {"1,John,Smith,John1989@gmail.com,20,88,79,59",
-                "2,Suzan,Erickson,Erickson_1990@gmailcom,19,91,72,85",
-                "3,Jack,Napoli,The_lawyer99@yahoo.com,19,85,84,87",
-                "4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
-                "5,Jeremy,Brown,jbro332@wgu.edu,27,88,92,84"};
-        
+           
+            new RosterProject();
+        }
       
-            public populateRoster()
+            public void RosterProject()
             {
                 for (int i = 0; i < students.length; i++)
                 {
@@ -28,7 +29,7 @@ public class RosterProject
                     String firstName = parts[1];
                     String lastName = parts[2];
                     String emailAddress = parts[3];
-                    int age = parts[4];
+                    int age = Integer.parseInt(parts[4]);
                     int testGrade1 = Integer.parseInt(parts[5]);
                     int testGrade2 = Integer.parseInt(parts[6]);
                     int testGrade3 = Integer.parseInt(parts[7]);
@@ -37,7 +38,9 @@ public class RosterProject
                     roster.add(x);
                 }
             }
-        
+       
+       
+    
         public class Student
         {
             public String studentID;
@@ -95,13 +98,14 @@ public class RosterProject
                      + ", " + testGrade2 + ", " + testGrade3 + "}");
             }
         }
-
+ public static class Roster 
+    {
         
         public static void remove(String studentID)
         {
-            for (int i = 0; i < roster.length; i++)
+            for (int i = 0; i < students.length; i++)
             {
-                if (roster.get(i).getStudentID().equals(studentID))
+                if (roster.get(i).getStudentID(studentID).equals(studentID))
                 {
                     roster.remove(i);
                     return;
@@ -145,4 +149,3 @@ public class RosterProject
             
         }
     }
-}
