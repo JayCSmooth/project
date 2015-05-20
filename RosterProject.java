8477192ae360946ca363170221bf0219167b026d
JayCSmooth <justchillin4life@gmail.com>
2015-05-20 17:58:06 -0400

    Update RosterProject.java

--- RosterProject.java
+++ RosterProject.java
@@ -103,16 +103,16 @@ public class RosterProject
         public static void add(String studentID, String firstName, String lastName, String emailAddress, int age, int testGrade1, int testGrade2, int testGrade3)
 		{
 		    populateRoster();
-		  }
+		}
 		  
-		  public static void populateRoster()
-		  {      
-		      roster.add(new Student("1", "John", "Smith", "John1989@gmail.com", 20, 88, 79, 59));
-              roster.add(new Student("2", "Suzan", "Erickson", "Erickson_1990@gmailcom", 19, 91, 72, 85));
-              roster.add(new Student("3", "Jack", "Napoli", "The_lawyer99@yahoo.com", 19, 85, 84, 87));
-              roster.add(new Student("4", "Erin", "Black", "Erin.black@comcast.net", 22, 91, 98, 82));
-              roster.add(new Student("5", "Chris", "Baker", "cbake55@wgu.edu", 29, 81, 78, 91));
-		    }  
+		public static void populateRoster()
+		{      
+			roster.add(new Student("1", "John", "Smith", "John1989@gmail.com", 20, 88, 79, 59));
+        		roster.add(new Student("2", "Suzan", "Erickson", "Erickson_1990@gmailcom", 19, 91, 72, 85));
+        		roster.add(new Student("3", "Jack", "Napoli", "The_lawyer99@yahoo.com", 19, 85, 84, 87));
+        		roster.add(new Student("4", "Erin", "Black", "Erin.black@comcast.net", 22, 91, 98, 82));
+        		roster.add(new Student("5", "Jeremy", "Brown", "jbro332@wgu.edu", 27, 88, 92, 84));
+		}  
 			
         
         
